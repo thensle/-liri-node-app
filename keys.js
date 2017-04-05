c49c641ae3dcd6e6ecb27a8f448ed55a987cf0ef
Tara Hensle <tara@northwestern.edu>
2017-04-04 20:08:20 -0500

    twitter api

--- keys.js
+++ keys.js
@@ -1,8 +1,6 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
   consumer_key: '0ewL3VduZqyh10RklSBHkJXkt',
   consumer_secret: 'A5XHr46phRVs78eWThc4CJfjHGdc8IbD9ByBQOYBW4fSkKnoou',
-  access_token_key: '	849398989732675584-jGBTdBaOY0TNfHkdYpijx7ona3LikWK',
+  access_token_key: '849398989732675584-jGBTdBaOY0TNfHkdYpijx7ona3LikWK',
   access_token_secret: 'h5WYdqXGCQiz8LHknmlvpjVUmP1Li0FJGMSkRjTz4SBhv'
-}
+};
--- liri.js
+++ liri.js
@@ -1,1 +1,49 @@
+//Utilizing packages for Node.js
+var Twitter = require("twitter");
+var spotify = require("spotify");
+// var request = require("require");
 
+//TWITTER API
+	//Grabbing API token keys from other JS file for Twitter package
+	var keys = require("./keys.js").twitterKeys;
+
+	//Passing the keys to the Twitter API
+	var tweets = new Twitter(keys);
+
+
+//Grab Tweets
+function myFavTweets(){
+	tweets.get('favorites/list', function(error, tweets, response) {
+		if(error){
+			console.log(error);
+	  	} else {
+	  		var availTweets;
+
+	  		//Determine number of tweets returned
+	  		if (tweets.length < 20){
+				availTweets = tweets.length
+	  		} else {
+	  			availTweets = 20	
+	  		};
+
+	  	  	for (var i = 0; i < availTweets; i++){
+	  	  		console.log('\n' + tweets[i].created_at);
+	  	  		console.log(tweets[i].text + '\n');
+	  	  	};
+		 }; 
+	});
+};
+
+//Grab Spotify information
+function spotifySong(){
+	var title = process.argv[3];
+
+	spotify.search({type:'track', query: title}, function(error, music){
+		if(error){
+			console.log(error);
+		} else {
+			
+		};
+	});
+
+};
