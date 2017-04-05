cac996c4b8d27e8b08102d29b0a6110b587289bb
Tara Hensle <tara@northwestern.edu>
2017-04-04 20:22:33 -0500

    spotify api and function

--- liri.js
+++ liri.js
@@ -10,6 +10,25 @@ var spotify = require("spotify");
 	//Passing the keys to the Twitter API
 	var tweets = new Twitter(keys);
 
+//Possible user inputs
+var command = process.argv[2].toLowerCase();
+
+switch(command){
+	case "my-tweets":
+		myFavTweets();
+		break;
+	case "spotify-this-song":
+		spotifySong();
+		break;
+	case "movie-this":
+		findMovie();
+		break;
+	case "do-what-it-says":
+		readFS();
+		break;
+	default:
+		console.log("Oops - not a validated command. Try again!\n\n" + "Options are: \n\n my-tweets\n spotify-this-song\n movie-this\n do-what-it-says");
+};
 
 //Grab Tweets
 function myFavTweets(){
@@ -36,14 +55,24 @@ function myFavTweets(){
 
 //Grab Spotify information
 function spotifySong(){
-	var title = process.argv[3];
+	if(process.argv.length > 4){
+		console.log(process.argv.length);
+		console.log("Provide the song in one string, please!");
+		return;
+	};
+
+	var title = process.argv[3].toLowerCase();
 
 	spotify.search({type:'track', query: title}, function(error, music){
 		if(error){
 			console.log(error);
 		} else {
-			
+			console.log(music);
 		};
 	});
 
 };
+
+function findMovie(){console.log("Yay!");};
+
+function readFS(){console.log("Yay!");};
